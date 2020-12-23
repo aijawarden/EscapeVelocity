b881a13ba65511e66920ad5b2d400010b1e505c0
aijawarden <76536027+aijawarden@users.noreply.github.com>
2020-12-23 12:22:27 -0800

    Add files via upload

---
+++ EscapeVelocity.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+public class EscapeVelocity 
+
+{
+
+G = 6.67 * 10-11 Nm2/kg2 //Gravitational Constant 
+M = 2.2 * 1014 kg //mass of Halley’s Comet
+R = 5,500 m //radius of Halley’s Comet
+
+calculateMoreMass(double velocity, long double* M);
+
+}
+
+public static void main(String[] args) 
+
+{
+
+}
